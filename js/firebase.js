37ebdc792c718bb881370c5272940b82fb01a6a3
sirajulummahacademy763-ui <sirajulummahacademy763@gmail.com>
2026-07-29 13:14:32 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,28 @@
 
+// Firebase Configuration
+
+const firebaseConfig = {
+
+    apiKey: "YOUR_API_KEY",
+
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+
+    projectId: "YOUR_PROJECT_ID",
+
+    storageBucket: "YOUR_PROJECT.appspot.com",
+
+    messagingSenderId: "XXXXXXXX",
+
+    appId: "XXXXXXXX"
+
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const db = firebase.firestore();
+
+const storage = firebase.storage();
